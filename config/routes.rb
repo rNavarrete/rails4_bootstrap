9c0e65fb1873f0dd0ab834098770f056a816d01f
Edgard R. Navarrete <kinglouis.belmont@gmail.com>
2013-09-04 00:02:31 -0700

    Added survey to the front page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 RailsBootstrap::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  resources :visitors, only: [:new, :create]
 
   # You can have the root of your site routed with "root"
   root :to => 'visitors#new'
